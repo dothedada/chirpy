e7c07061ab0f86dd40b373792533dd2a6662f671
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-08-17 11:43:38 -0500

    Adds server status path

--- main.go
+++ main.go
@@ -6,12 +6,22 @@ import (
 	"net/http"
 )
 
+func handlerServerStatus(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	const port = "8080"
 	const fileRoot = "."
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(fileRoot)))
+	mux.Handle(
+		"/app/",
+		http.StripPrefix("/app/", http.FileServer(http.Dir(fileRoot))),
+	)
+	mux.HandleFunc("/healthz", handlerServerStatus)
 
 	server := &http.Server{
 		Addr:    ":" + "8080",
