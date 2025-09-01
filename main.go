0cf1528bd7607973d541436e9c0ed2de5d8dee3e
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-08-31 20:57:24 -0500

    Some chit

--- main.go
+++ main.go
@@ -22,44 +22,6 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
-type reqError struct {
-	status int
-	error  error
-}
-
-func isValidRequest(req *http.Request) reqError {
-	if !strings.HasPrefix(req.Header.Get("Content-Type"), "application/json") {
-		return reqError{
-			status: http.StatusBadRequest,
-			error:  errors.New("Something went wrong"),
-		}
-	}
-
-	type body struct {
-		Text string `json:"body"`
-	}
-	var reqBody body
-	defer req.Body.Close()
-
-	decoder := json.NewDecoder(req.Body)
-	err := decoder.Decode(&reqBody)
-	if err != nil {
-		return reqError{
-			status: http.StatusBadRequest,
-			error:  errors.New("Something went wrong"),
-		}
-	}
-
-	if len(reqBody.Text) > 140 {
-		return reqError{
-			status: http.StatusBadRequest,
-			error:  errors.New("Chirp is too long"),
-		}
-	}
-
-	return reqError{}
-}
-
 func handlerChirpValidation(w http.ResponseWriter, req *http.Request) {
 	type resError struct {
 		Error string `json:"error"`
